9aa3f902b80fbe2d1eda13b292fbb50bd5ef242c
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-01-07 14:19:50 +0000

    Data types

---
+++ datacleaningutils/data_types.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import datetime
+def rough_heuristic_column_type_check(column, metadata_type):
+    """
+    This function is a very quick heuristic/rule of thumb that
+    should check the data type of a column
+
+    It will spot errors a good proportion of the time, but you will likely experience false positives and false negatives
+
+    It will raise a ValueError if it finds a problem
+
+    Args:
+        columns: A pandas seris
+        metadata_type:  The metadata type, see https://github.com/moj-analytical-services/data_engineering_utils/blob/master/dataengineeringutils/data/data_type_conversion.csv
+
+    Returns:
+        None
+
+    """
+    # The type of NaN is float so any column with a None in will be float, even if all other numbers are ints
+    type_checks = {
+        "character" : {type(None), str},
+        "int": {np.int64, np.float64},
+        "float": {np.float64},
+        "date": {datetime.date, type(None), pd._libs.tslib.NaTType}
+    }
+
+    value_checks = {
+        "character" : {},
+        "int": {pd.np.nan},
+        "float": {pd.np.nan},
+        "date": {pd.NaT, pd.np.nan}
+    }
+
+    this_column = column.sample(10, replace=True)
+
+    possible_types = type_checks[metadata_type]
+    possible_values = value_checks[metadata_type]
+
+    for val in this_column:
+        if type(val) not in possible_types and val not in possible_values:
+            message = "The value {} in column {} is of type {} which is not in possible types {}"
+            message = message.format(val, column.name, type(val), possible_types)
+            raise ValueError(message)
+
---
+++ tests/test_data_types.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests
+"""
+import datetime
+import unittest
+import pandas as pd
+from datacleaningutils.data_types import rough_heuristic_column_type_check
+
+class DataTypeTest(unittest.TestCase):
+    """
+    Test the add function
+    """
+
+    def test_data_types_1(self):
+        """
+        Test first row of dates.csv is successfully converted
+        """
+
+        date = datetime.date(2017,1,1)
+
+        df = pd.DataFrame({"a": ["a", None, ""],
+                            "b": [1,None,3],
+                            "c":["2017-01-01", "2017-02-01", ""],
+                            "d": [datetime.date(2017,1,1), datetime.date(2017,6,4), None],
+                            "e": [1.1,None,1.3]})
+
+        df["c"] =  pd.to_datetime(df["c"]).dt.date
+
+        rough_heuristic_column_type_check(df["a"], "character")
+        rough_heuristic_column_type_check(df["b"], "int")
+        rough_heuristic_column_type_check(df["c"], "date")
+        rough_heuristic_column_type_check(df["d"], "date")
+        rough_heuristic_column_type_check(df["e"], "float")
+
+
+if __name__ == '__main__':
+    unittest.main()
