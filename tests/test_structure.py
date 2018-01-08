a9d234af383efb33274d90fcbd1494fa770b02e5
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-01-08 09:23:53 +0000

    Test structure function

--- tests/test_structure.py
+++ tests/test_structure.py
@@ -24,10 +24,38 @@ class StructureTest(unittest.TestCase):
             'field_3': ["a", "b"]
         }
         df = pd.DataFrame(data)
+        df = df[["field_1", "field_2", "field_3"]]
 
         check_compatible_with_metadata(df, "tests/data/meta1.json")
 
+    def test_check_compatible_with_metadata_2(self):
+        """
+        Check it fails if we incorrectly specify date types
+        """
 
+        data = {
+            'field_1': ["a", "b"],
+            'field_2': ["a", "b"],
+            'field_3': ["a", "b"],
+            'field_4': ["a", "b"]
+        }
+
+        df = pd.DataFrame(data)
+        df = df[["field_1", "field_2"]]
+        with self.assertRaises(ValueError):
+            check_compatible_with_metadata(df, "tests/data/meta1.json")
+
+
+        df = pd.DataFrame(data)
+        df = df[["field_1", "field_2", "field_3", "field_4"]]
+        with self.assertRaises(ValueError):
+            check_compatible_with_metadata(df, "tests/data/meta1.json")
+
+
+        df = pd.DataFrame(data)
+        df = df[["field_2", "field_1", "field_3"]]
+        with self.assertRaises(ValueError):
+            check_compatible_with_metadata(df, "tests/data/meta1.json")
 
 if __name__ == '__main__':
     unittest.main()
